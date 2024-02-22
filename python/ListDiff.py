25491125538aaa35baa66447c8692f795422c02b
Ozgur Yuksel <ozgur.yuksel@bisoft.com.tr>
2024-02-22 15:15:52 +0300

    cleanup

--- python/ListDiff.py
+++ python/ListDiff.py
@@ -1,24 +1,18 @@
-import sys
-import csv
+import sys,csv
 
 def csvline2list(file_path):
     with open(file_path, "r", newline='') as csvlinefile:
-        csvline_reader = csv.reader(csvlinefile)
-        for row in csvline_reader:
+        for row in csv.reader(csvlinefile):
             return row
     
-
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python ListDiff.py <file1> <file2>")
         sys.exit(1)
 
-    file1 = sys.argv[1]
-    file2 = sys.argv[2]
-    list1 = csvline2list(file1)
-    list2 = csvline2list(file2)
+    list1 = csvline2list(sys.argv[1])
+    list2 = csvline2list(sys.argv[2])
     difference = [item for item in list2 if item not in list1]
-    
     for item in difference:
         print(item, end=",")
     
